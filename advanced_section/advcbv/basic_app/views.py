39897b0c2551e4b0320a997ad05247a7248deafe
Cuong Nguyen <cuong.nguyenduy@gmail.com>
2017-10-25 22:42:55 +0800

    Class Based View - Change index method to CBView class - Change the urlpatterns to call views.CBView.as_view()

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
-
-
+class CBView(View):
+    def get(self, request):
+        return HttpResponse('<h1>Hello, World! Index Page Home with CBView</h1>')
