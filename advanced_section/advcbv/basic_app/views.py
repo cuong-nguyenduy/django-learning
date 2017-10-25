e4eaf7c25ac14d09286c1e4a520970bac0135b40
Cuong Nguyen <cuong.nguyenduy@gmail.com>
2017-10-25 23:04:12 +0800

    Inject context into CBV

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -5,3 +5,8 @@ from django.views.generic.base import TemplateView
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'BASIC INJECTION!'
+        return context
