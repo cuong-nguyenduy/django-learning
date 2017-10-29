7e78f7b5f58e8221fa1054b87b6603ea425fb939
Cuong Nguyen <cuong.nguyenduy@gmail.com>
2017-10-29 11:04:45 +0800

    CRUD - Using CreateView, UpdateView, DeleteView to update School data
    Changes
    - Add CBV for each operation (Create, Update, Delete)
    - Add template file 'school_form.html' for school update/create
    - Add tempalte file 'school_confirm_delete.html' for detete
    - Update other tempates to have links to correct views
    - Update urls.py

--- advanced_section/advcbv/basic_app/urls.py
+++ advanced_section/advcbv/basic_app/urls.py
@@ -6,4 +6,7 @@ app_name = 'basic_app'
 urlpatterns = [
     url(r'^$', views.SchoolListView.as_view(), name='list'),
     url(r'^(?P<pk>\d+)/$', views.SchoolDetailView.as_view(), name='detail'),
+    url(r'^create/$', views.SchoolCreateView.as_view(), name='school_create'),
+    url(r'^update/(?P<pk>\d+)/$', views.SchoolUpdateView.as_view(), name='school_update'),
+    url(r'^delete/(?P<pk>\d+)/$', views.SchoolDeleteView.as_view(), name='school_delete'),
 ]
--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic.base import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.views.generic import CreateView, UpdateView, DeleteView
 from . import models
 
 
@@ -20,3 +22,18 @@ class SchoolDetailView(DetailView):
     context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
+
+
+class SchoolCreateView(CreateView):
+    fields = '__all__'
+    model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy('basic_app:list')
