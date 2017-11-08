33aff09e11bc2ab28658c73435047c07d3d6fb25
Cuong Nguyen <cuong.nguyenduy@gmail.com>
2017-11-08 12:36:02 +0800

    Simple Social Clone Project - Enable django-debug-toolbar for debugging

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'bootstrap3',
     'accounts',
     'groups',
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'simplesocial.urls'
@@ -134,3 +136,8 @@ STATICFILES_DIRS = [
 # Login and Logout
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
+
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
